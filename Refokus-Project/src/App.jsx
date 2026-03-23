ccf7b13e650e29e4ae46da8752b36ae99cc8d009
krishna <krishnacodes17@gmail.com>
2026-03-23 18:43:34 +0530

    infinte Components done

--- Refokus-Project/src/App.jsx
+++ Refokus-Project/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import NavBar from './Components/NavBar'
 import Work from './Components/Work'
 import Stripes from './Components/Stripes'
+import Infinite from './Components/Infinite'
 
 function App() {
   return (
     <div className=' bg-zinc-800 text-white'>
       <NavBar/>
       <Work/>
-      {/* <Stripes/> */}
+      <Infinite />
     </div>
   ) 
 }
---
+++ Refokus-Project/src/Components/Infinite.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './infinite.css'
+
+function Infinite() {
+  return (
+    <div class="page5" data-scroll >
+      <div class="elem">
+        <h1>
+          <span>Sport</span> - Fasion - Technology -
+        </h1>
+        <h1>
+          <span>Sport</span> - Fasion - Technology -
+        </h1>
+      </div>
+
+      <div class="elem2">
+        <h1>
+          Science - <span>Partners</span> - Travel -
+        </h1>
+        <h1>
+          Science - <span>Partners</span> - Travel -
+        </h1>
+      </div>
+
+
+     
+    </div>
+  );
+}
+
+export default Infinite;
